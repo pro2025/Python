52a9e6f24ee3e0ed595bf9fce8a9e12c2d7ba0d7
T Nikhitha <119213327+pro2025@users.noreply.github.com>
2023-06-17 16:50:51 +0530

    help function in python

---
+++ help.py
@@ -0,0 +1,46 @@
+'''
+help function will give information of given function...
+'''
+help("if")
+
+'''
+to the function of while function...
+'''
+help("while")
+
+'''
+to know the function of string...
+'''
+help("string")
+
+'''
+to check the number of keywords in python latest version...
+'''
+help("keywords")
+
+'''
+To generate help on these keywords import keyword...
+'''
+help("keyword.kwlist")
+
+
+
+
+'''
+dir() tells number of variables and integer from created variable...
+'''
+var1 = 10
+print(var1)
+
+'''
+dir() function use...
+'''
+dir(var1)
+
+
+
+'''
+Unique identification number of object...
+'''
+id(var1)
+
