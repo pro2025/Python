940e0bde7133e4241b8aa568e4622562f29fd294
T Nikhitha <119213327+pro2025@users.noreply.github.com>
2023-06-17 17:32:55 +0530

    Hello Python

---
+++ hello.py
@@ -0,0 +1,18 @@
+'''
+printing hello world...
+'''
+print("Welcome to the world of Python")
+
+'''
+function to print hello...
+'''
+def test ():
+    print("Hello")
+print(test())
+
+'''
+variables..
+'''
+var1 = 5
+print(var1)
+
