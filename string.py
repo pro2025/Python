61f1c4c746f53e7366f5cf284a93cb1a6ed1d629
T Nikhitha <119213327+pro2025@users.noreply.github.com>
2023-06-28 15:35:46 +0530

    string examples

---
+++ string.py
@@ -0,0 +1,25 @@
+st = "string demo"
+print(st)
+st = "PYTHON"
+print(st)
+st = "Hello"
+print(st)
+print(st[0])
+print(st[1])
+print(st[2])
+print(st[3])
+print(st[4])
+print(id(st))
+print(st[2])
+
+"""
+to get string in different lines
+"""
+st = '''welcome
+to
+the
+world
+of 
+python
+'''
+print(st)
